a96e17d2cb3e5b8f73a8610d88c0bb403ccd6758
tsemach <tsemach.mizrachi>
2018-06-22 00:19:01 +0300

    add viewer

---
+++ src/app/python/viewer/viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-python-viewer',
+  templateUrl: './viewer.component.html',
+  styleUrls: ['./viewer.component.css']
+})
+export class PythonViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
